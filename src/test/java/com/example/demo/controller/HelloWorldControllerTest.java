92cc8fb699281e67f614a5ed25918d20be51200f
Yuki Hirai <yukihirai0505@gmail.com>
2019-10-16 15:40:40 +0900

    Add MVC Mock test for Hello World Controller

---
+++ src/test/java/com/example/demo/controller/HelloWorldControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloWorldController.class)
+public class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void helloWorld_basic() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/hello-world")
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request).andReturn();
+        assertEquals("Hello World", result.getResponse().getContentAsString());
+    }
+}
