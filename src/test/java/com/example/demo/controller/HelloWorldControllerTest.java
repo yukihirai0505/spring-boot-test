dc7b2da5d1597683b2268ffc476c363461a90ec8
Yuki Hirai <yukihirai0505@gmail.com>
2019-10-16 15:56:36 +0900

    Add response matchers to check status and content

--- src/test/java/com/example/demo/controller/HelloWorldControllerTest.java
+++ src/test/java/com/example/demo/controller/HelloWorldControllerTest.java
@@ -12,6 +12,8 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HelloWorldController.class)
@@ -25,7 +27,10 @@ public class HelloWorldControllerTest {
         RequestBuilder request = MockMvcRequestBuilders
                 .get("/hello-world")
                 .accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(request).andReturn();
-        assertEquals("Hello World", result.getResponse().getContentAsString());
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"))
+                .andReturn();
+//        assertEquals("Hello World", result.getResponse().getContentAsString());
     }
 }
