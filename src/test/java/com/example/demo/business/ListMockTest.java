a07dc99964544e67bd5d13ab934f3d5a0383d48c
Yuki Hirai <yukihirai0505@gmail.com>
2019-10-16 14:14:57 +0900

    Add ListMockTest

---
+++ src/test/java/com/example/demo/business/ListMockTest.java
@@ -0,0 +1,39 @@
+package com.example.demo.business;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ListMockTest {
+
+    @Mock
+    List mock;
+
+    @Test
+    public void size_basic() {
+        when(mock.size()).thenReturn(5);
+        assertEquals(5, mock.size());
+    }
+
+
+    @Test
+    public void returnDifferentValues() {
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5, mock.size());
+        assertEquals(10, mock.size());
+    }
+
+    @Test
+    public void returnWithParameters() {
+        when(mock.get(0)).thenReturn("in28Minutes");
+        assertEquals("in28Minutes", mock.get(0));
+        assertEquals(null, mock.get(1));
+    }
+}
