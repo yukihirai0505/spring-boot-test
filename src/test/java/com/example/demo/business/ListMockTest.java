05c5d58ba078d2149163990c6aea0fd10873fdaa
Yuki Hirai <yukihirai0505@gmail.com>
2019-10-16 14:32:08 +0900

    Verify method calls

--- src/test/java/com/example/demo/business/ListMockTest.java
+++ src/test/java/com/example/demo/business/ListMockTest.java
@@ -10,13 +10,13 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ListMockTest {
 
     @Mock
-    List mock;
+    List<String> mock;
 
     @Test
     public void size_basic() {
@@ -45,4 +45,18 @@ public class ListMockTest {
         assertEquals("in28Minutes", mock.get(0));
         assertEquals("in28Minutes", mock.get(10));
     }
+
+    @Test
+    public void verificationBasics() {
+        String value1 = mock.get(0);
+        String value2 = mock.get(1);
+
+        verify(mock).get(0);
+        verify(mock, times(2)).get(anyInt());
+        verify(mock, atLeast(1)).get(anyInt());
+        verify(mock, atLeastOnce()).get(anyInt());
+        verify(mock, atMost(2)).get(anyInt());
+        verify(mock, never()).get(2);
+    }
+
 }
