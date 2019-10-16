67d3c38febbca133af29ff3690373dba15c70863
Yuki Hirai <yukihirai0505@gmail.com>
2019-10-16 13:22:05 +0900

    Add mock test by using Mockito

---
+++ src/test/java/com/example/demo/business/SomeBusinessMockTest.java
@@ -0,0 +1,38 @@
+package com.example.demo.business;
+
+import com.example.demo.data.SomeDataService;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SomeBusinessMockTest {
+
+    SomeBusinessImpl business = new SomeBusinessImpl();
+    SomeDataService dataServiceMock = mock(SomeDataService.class);
+
+    @Before
+    public void before() {
+        business.setSomeDataService(dataServiceMock);
+    }
+
+    @Test
+    public void calculateSumUsingDataService_basic() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{1, 2, 3});
+        assertEquals(6, business.calculateSumUsingDataService());
+    }
+
+    @Test
+    public void calculateSumUsingDataService_empty() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{});
+        assertEquals(0, business.calculateSumUsingDataService());
+    }
+
+    @Test
+    public void calculateSumUsingDataService_oneValue() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{1});
+        assertEquals(1, business.calculateSumUsingDataService());
+    }
+}
